e309de80f2f1439e5c1e6b4e6822525edb3eff52
menayangbenita <menayangbenita@gmail.com>
2025-04-10 07:19:06 +0700

    fix image landing

--- resources/views/landing/dokter-kami.blade.php
+++ resources/views/landing/dokter-kami.blade.php
@@ -158,7 +158,7 @@
     <!-- Vendor JS Files -->
     <script src="{{ asset('landing/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('landing/vendor/php-email-form/validate.js') }}"></script>
-    <script src="{{ asset('landing/vendor/aos/aos.jsa') }}"></script>
+    <script src="{{ asset('landing/vendor/aos/aos.js') }}"></script>
     <script src="{{ asset('landing/vendor/glightbox/js/glightbox.min.js') }}"></script>
     <script src="{{ asset('landing/vendor/swiper/swiper-bundle.min.js') }}"></script>
     <script src="{{ asset('landing/vendor/imagesloaded/imagesloaded.pkgd.min.js') }}"></script>
